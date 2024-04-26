86410f5a07b9fa927603a09d5f84bd34a86382af
Muhammad Gabriel Somoal <95246662+protoga@users.noreply.github.com>
2024-04-26 16:41:52 +0000

    Commit made from VS Code Azure Static Web Apps, Bismillah Bisa Selesai

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Online Shop of Queen Shop",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Collaboration to create a simple application to help queen shop owner in doing her business management.",
+    url: "https://github.com/protoga/Olshop",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Image Classification",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Assist in detecting each image with the help of AI so that it can run quickly.",
+    url: "https://github.com/protoga/Image-Classification",
   },
 ];
 
