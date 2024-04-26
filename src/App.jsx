b5401ee3337c9b6de9933fe499fa81ed1f8fb757
Muhammad Gabriel Somoal <95246662+protoga@users.noreply.github.com>
2024-04-26 17:00:20 +0000

    enhance: perubahan name

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Muhammad Gabriel Somoa",
+  name: "Muhammad Gabriel Somoal",
   title: "President & Machine Learning Engineer",
   email: "MuhammadGabriel.Somoal@studentambassadors.com",
   gitHub: "protoga",
